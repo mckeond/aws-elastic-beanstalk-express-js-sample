cf2c51c0d99364c543785c95d318f215c3707e17
mckeond <93330480+mckeond@users.noreply.github.com>
2022-02-07 15:29:00 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Its time to pick up Virginia!'));
+app.get('/', (req, res) => res.send('I went to pick up Virginia!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
